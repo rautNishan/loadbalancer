20f3167a554131da6d4c2bab8d13f291f9865644
Nishan Raut <nishan.raut1709@gmail.com>
2025-05-22 13:14:47 +0545

    Accept Tcp Connection

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+)
+
+func forwardConnection(conn net.Conn, backendList []string) {
+
+}
 
 func main() {
-	backendList := [3]string{"localhost:3001", "localhost:3002", "localhost:3003"}
-	for i := 0; i < len(backendList); i++ {
-		fmt.Println(backendList[i])
+	backendList := []string{"localhost:3001", "localhost:3002", "localhost:3003"}
+	// Accept tcp connection
+	listner, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listner.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go forwardConnection(conn, backendList)
 	}
 }
