3f3d983b0ebed79ef16be09343db13bad1836972
Nishan Raut <nishan.raut1709@gmail.com>
2025-05-31 21:23:57 +0545

    Optimized

--- main.go
+++ main.go
@@ -14,31 +14,7 @@ type Backend struct {
 	n       uint64
 }
 
-func copyData(destination net.Conn, source net.Conn, wg *sync.WaitGroup) {
-	defer wg.Done()
-	buffer := make([]byte, 4*1024)
-	for {
-		//Close the connection with client
-		_ = source.SetReadDeadline(time.Now().Add(20 * time.Second))
-		n, err := source.Read(buffer)
-		if err != nil {
-			fmt.Println("Error: ", err)
-			break
-		}
-		_, err = destination.Write(buffer[:n])
-		if err != nil {
-			fmt.Println("Error : ", err)
-			break
-		}
-	}
-	tcpConn, isTCP := destination.(*net.TCPConn)
-	if isTCP {
-		tcpConn.CloseWrite()
-	}
-}
-
 func RoundRobin(backend *Backend) string {
-	fmt.Println(backend.n)
 	i := atomic.AddUint64(&backend.n, 1)
 	return backend.servers[i%uint64(len(backend.servers))]
 }
@@ -50,44 +26,67 @@ func (backend *Backend) Choose(strategy string) string {
 	default:
 		return ""
 	}
+
+}
+
+func copyData(destinationConn net.Conn, sourceConn net.Conn, wg *sync.WaitGroup, path string) {
+	buffer := make([]byte, 1024)
+	fmt.Println("Ready to read")
+	defer wg.Done()
+	for {
+		_ = sourceConn.SetReadDeadline(time.Now().Add(10 * time.Second))
+		n, err := sourceConn.Read(buffer)
+		// fmt.Println("Complete Read: ", string(buffer[:n]))
+		if err != nil {
+			fmt.Println("This is error in read", path, err)
+			break
+		}
+		_, err = destinationConn.Write(buffer[:n])
+		if err != nil {
+			fmt.Println("This is error in write: ", path, err)
+			break
+		}
+	}
+	tcpConn, isTcp := destinationConn.(*net.TCPConn)
+	if isTcp {
+		tcpConn.CloseWrite()
+	}
 }
 
 func forwardConnection(clientConn net.Conn, backend *Backend) {
 	backendAddress := backend.Choose("round-robin")
+	// fmt.Println("This is choosen adddress: ", backendAddress)
+
 	serverConn, err := net.Dial("tcp", backendAddress)
-
-	if err != nil {
-		log.Println("copyData error:", err)
-	}
-
 	defer func() {
 		serverConn.Close()
 		clientConn.Close()
-		fmt.Println("Connection Closed")
+		fmt.Println("Connection closed")
 	}()
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	var wg sync.WaitGroup
 	wg.Add(2)
-	go copyData(serverConn, clientConn, &wg)
-	go copyData(clientConn, serverConn, &wg)
+	go copyData(serverConn, clientConn, &wg, "client->backend")
+	go copyData(clientConn, serverConn, &wg, "backend->client")
 	wg.Wait()
-	fmt.Println("Wait Completes")
 }
 
 func main() {
-	backendList := []string{"localhost:3001", "localhost:3002", "localhost:3003"}
-	// Accept tcp connection
+	backendLists := []string{"localhost:3001", "localhost:3002", "localhost:3003"}
 	listner, err := net.Listen("tcp", ":3000")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer listner.Close()
-	//Pointer to backend
-	b := &Backend{servers: backendList, n: 0}
+	fmt.Println("Ready to accept connection")
+	b := &Backend{servers: backendLists, n: 0}
 	for {
 		conn, err := listner.Accept()
+		fmt.Println(conn)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println(err)
 		}
 		go forwardConnection(conn, b)
 	}
